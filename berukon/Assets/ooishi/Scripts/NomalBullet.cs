f836851380f2bdb1c702a619099d90cf949868cc
大石　隼 <>
2019-12-18 21:29:08 +0900

    ユニットロックオン完了、距離ソート途中

---
+++ berukon/Assets/ooishi/Scripts/NomalBullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NomalBullet : MonoBehaviour
+{
+    private GameObject target;
+    public float speed;
+    private UnitShot unit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        unit = gameObject.transform.parent.GetComponent<UnitShot>();
+        target= unit.target;
+        gameObject.transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(unit.target.transform.position - transform.position), 0.3f);
+        //targetに向かって進む
+        transform.position =Vector3.Lerp(transform.position, target.transform.position,speed);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ berukon/Assets/ooishi/Scripts/UnitShot.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitShot : MonoBehaviour
+{
+    public GameObject shot;
+    public GameObject target;
+    public float shotTime;
+    private float time;
+    private bool hitfrag;
+    private GameObject[] enemys;
+    private GameObject enemysave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+        hitfrag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shot();
+    }
+    void Shot()
+    {
+        time += Time.deltaTime;
+
+        if (time >= shotTime&&hitfrag)
+        {
+            // Do anything
+
+            time = 0.0f;
+            // プレハブからインスタンスを生成
+            GameObject obj = Instantiate(shot, transform.position, Quaternion.identity);
+            obj.transform.parent = transform;
+        }
+    }
+
+    void Sort()
+    {
+        if(enemys.Length!=0)
+        {
+            for (int i = 0; i < enemys.Length; i++)
+            {
+                float distance = Vector2.Distance(transform.position, enemys[i].transform.position);
+                float distance = Vector2.Distance(transform.position, target.transform.position);
+                if (enemys[i].gameObject.transform.position)
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="Enemy")
+        {
+            if(hitfrag==false)
+            {
+                hitfrag = true;
+            }
+            target = collision.gameObject;
+        }
+    }
+}

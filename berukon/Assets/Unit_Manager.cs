497b937f24b2bf0d9569db3d3b2e4713f41210c1
猪瀬太一 <zigokunohonou0919@gmail.com>
2019-12-20 01:47:05 +0900

    hp関連　ディクショナリ修正待ち

---
+++ berukon/Assets/Unit_Manager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit_Manager : MonoBehaviour
+{
+    public int UnityLife = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void UnitLife_Manager(int Life)
+    {
+        UnityLife = UnityLife + Life;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Heal")
+        {
+            UnitLife_Manager(1);
+        }
+    }
+}

64548c011fe3afdb79842df90e7ed4ea88e0896f
猪瀬太一 <zigokunohonou0919@gmail.com>
2019-12-19 15:57:22 +0900

    ウェーブ系統の完成

---
+++ berukon/Assets/EnemyMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rb;
+    public float x;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(x, 0);
+
+
+    }
+}
---
+++ berukon/Assets/Wave_Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave_Manager : MonoBehaviour
+{
+    public List<GameObject> EnemyList = new List<GameObject>();
+    private float x;
+    public float purasu;
+    void Start()
+    {
+        x= 0;
+            foreach(GameObject gb in EnemyList)
+        {
+            Instantiate(gb, new Vector2(transform.position.x + x, transform.position.y), Quaternion.identity);
+            x+=purasu;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

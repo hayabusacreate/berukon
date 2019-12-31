371dd4610a78b3e789a22d2662ec8831e31b13bb
大石　隼 <>
2019-12-31 13:04:34 +0900

    エネミードローンアニメーションさせた

---
+++ berukon/Assets/ooishi/Scripts/EnemyAnimation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    public float y;
+    private float count;
+    private bool flag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flag = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        if(flag)
+        {
+            count++;
+            if (count > y)
+            {
+                flag = false;
+            }
+        }
+        else
+        {
+            count--;
+            if (count < -y)
+            {
+                flag = true;
+            }
+        }
+
+        gameObject.transform.position=Vector3.Lerp(transform.position,new Vector3(transform.position.x,transform.position.y+count/100,transform.position.z),1) ;
+    }
+}

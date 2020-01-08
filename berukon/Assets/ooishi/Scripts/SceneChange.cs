b948e38cd2b71970fedb70171551d1bd3b62e9df
大石　隼 <>
2020-01-08 11:16:46 +0900

    a

--- berukon/Assets/ooishi/Scripts/SceneChange.cs
+++ berukon/Assets/ooishi/Scripts/SceneChange.cs
@@ -15,10 +15,11 @@ public class SceneChange : MonoBehaviour
     public Scene scene;
     public Core_Manager core;
     public Wave wave;
+    public int stagenum;
     // Start is called before the first frame update
     void Start()
     {
-        
+        stagenum = 0;
     }
 
     // Update is called once per frame
@@ -37,6 +38,14 @@ public class SceneChange : MonoBehaviour
         }
         if (scene == Scene.StageSlect)
         {
+            if(Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                stagenum--;
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                stagenum++;
+            }
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 SceneManager.LoadScene("test");
---
+++ berukon/Assets/ooishi/Scripts/SerectAnim.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerectAnim : MonoBehaviour
+{
+    private SceneChange sceneChange;
+    private int stage;
+    private Animator anim;
+    private float speed;
+    private bool changeflag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        sceneChange = transform.parent.gameObject.GetComponent<SceneChange>();
+        stage = sceneChange.stagenum;
+        speed = 0;
+        anim.speed = speed;
+        anim.SetBool("LR", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stage>sceneChange.stagenum)
+        {
+            if(!changeflag)
+            {
+                changeflag = true;
+                speed = 2;
+            }
+            else
+            {
+                speed -= 0.01f;
+                anim.speed = speed;
+                anim.SetBool("LR", true);
+                if (speed < 0)
+                {
+                    changeflag = false;
+                    stage = sceneChange.stagenum;
+                }
+            }
+        }
+        if (stage < sceneChange.stagenum)
+        {
+            if (!changeflag)
+            {
+                changeflag = true;
+                speed = 2;
+            }
+            else
+            {
+                speed -= 0.01f;
+                anim.speed = speed;
+                anim.SetBool("LR", false);
+                if (speed < 0)
+                {
+                    changeflag = false;
+                    stage = sceneChange.stagenum;
+                }
+            }
+        }
+    }
+}

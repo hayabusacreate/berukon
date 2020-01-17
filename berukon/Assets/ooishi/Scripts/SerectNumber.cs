5c3e2e969c5c763879453792dffd178c1456cd7c
大石　隼 <>
2020-01-18 00:31:30 +0900

    セレクトステージが何ステージか見えるように

---
+++ berukon/Assets/ooishi/Scripts/SerectNumber.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerectNumber : MonoBehaviour
+{
+    public GameObject serect;
+    public int num;
+    private SceneChange sceneChange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneChange = gameObject.transform.parent.gameObject.GetComponent<SceneChange>();
+        serect.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(sceneChange.stagenum+1==num)
+        {
+            serect.SetActive(true);
+        }else
+        {
+            serect.SetActive(false);
+        }
+    }
+}

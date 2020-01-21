e308331ce7e23ef6b8411b0d4f15deb7f9f2686e
大石　隼 <>
2020-01-22 06:45:18 +0900

    死亡時の演出土台

---
+++ berukon/Assets/ooishi/Scripts/HitEffect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitEffect : MonoBehaviour
+{
+    public GameObject hit, safe;
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hit.SetActive(false);
+        safe.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x<-3)
+        {
+            safe.SetActive(true);
+        }
+        else
+        {
+            hit.SetActive(true);
+        }
+        time += Time.deltaTime;
+        if(time>1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

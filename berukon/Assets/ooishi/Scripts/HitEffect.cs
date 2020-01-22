c5d8e1f4e42d2702a82db4e89c0cf4e027443df6
大石　隼 <>
2020-01-23 02:18:19 +0900

    エクセレント君たち差し込み

--- berukon/Assets/ooishi/Scripts/HitEffect.cs
+++ berukon/Assets/ooishi/Scripts/HitEffect.cs
@@ -4,28 +4,49 @@ using UnityEngine;
 
 public class HitEffect : MonoBehaviour
 {
-    public GameObject hit, safe;
+    public GameObject nice,great,exe, safe;
     private float time;
+    private Effect effect;
+    private bool change;
     // Start is called before the first frame update
     void Start()
     {
-        hit.SetActive(false);
+        effect = GameObject.Find("Effect").GetComponent<Effect>();
+        nice.SetActive(false);
+        great.SetActive(false);
+        exe.SetActive(false);
         safe.SetActive(false);
+        change = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x<-3)
+        if(change==false)
         {
-            safe.SetActive(true);
-        }
-        else
-        {
-            hit.SetActive(true);
+            if (transform.position.x < -3)
+            {
+                safe.SetActive(true);
+            }
+            if (effect.effect == 0)
+            {
+                effect.effect++;
+                nice.SetActive(true);
+            }
+            else if (effect.effect == 1)
+            {
+                effect.effect++;
+                great.SetActive(true);
+            }
+            else if (effect.effect >= 2)
+            {
+                effect.effect++;
+                exe.SetActive(true);
+            }
+            change = true;
         }
         time += Time.deltaTime;
-        if(time>1)
+        if(time>0.75f)
         {
             Destroy(gameObject);
         }
